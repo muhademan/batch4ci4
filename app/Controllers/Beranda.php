c3d52b3914e569a55fe2d8be1dcc24ea4fa058dc
muhademan <ade11rahman@gmail.com>
2020-12-19 16:01:17 +0700

    memakai framework ci 4

---
+++ app/Controllers/Beranda.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Beranda extends BaseController
+{
+    function home()
+    {
+        return view('home');
+    }
+}
---
+++ app/Views/home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Berita</title>
+    <!-- bootstrap ci css -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Portal Berita</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <header class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="h1"> Portal Berita CodeIgniter</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 card">
+                <h5 class="h5">Berita Pertama Judul</h5>
+                <p>Bacon ipsum dolor amet corned beef swine burgdoggen, spare ribs short ribs pork porchetta venison t-bone pancetta beef ribeye. Pastrami frankfurter kielbasa flank, prosciutto tongue short loin alcatra filet mignon strip steak doner shank. Turducken shank meatloaf, swine cupim pork filet mignon jowl cow pig jerky tenderloin alcatra. Jerky buffalo boudin kevin. Pork loin pastrami brisket alcatra ground round t-bone burgdoggen leberkas shankle swine hamburger. Pastrami boudin buffalo short ribs strip steak ribeye turkey beef meatball pancetta biltong jowl rump prosciutto.</p>
+            </div>
+            <div class="col-md-12 card">
+                <h5 class="h5">Berita Pertama Judul</h5>
+                <p>Bacon ipsum dolor amet corned beef swine burgdoggen, spare ribs short ribs pork porchetta venison t-bone pancetta beef ribeye. Pastrami frankfurter kielbasa flank, prosciutto tongue short loin alcatra filet mignon strip steak doner shank. Turducken shank meatloaf, swine cupim pork filet mignon jowl cow pig jerky tenderloin alcatra. Jerky buffalo boudin kevin. Pork loin pastrami brisket alcatra ground round t-bone burgdoggen leberkas shankle swine hamburger. Pastrami boudin buffalo short ribs strip steak ribeye turkey beef meatball pancetta biltong jowl rump prosciutto.</p>
+            </div>
+            <div class="col-md-12 card">
+                <h5 class="h5">Berita Pertama Judul</h5>
+                <p>Bacon ipsum dolor amet corned beef swine burgdoggen, spare ribs short ribs pork porchetta venison t-bone pancetta beef ribeye. Pastrami frankfurter kielbasa flank, prosciutto tongue short loin alcatra filet mignon strip steak doner shank. Turducken shank meatloaf, swine cupim pork filet mignon jowl cow pig jerky tenderloin alcatra. Jerky buffalo boudin kevin. Pork loin pastrami brisket alcatra ground round t-bone burgdoggen leberkas shankle swine hamburger. Pastrami boudin buffalo short ribs strip steak ribeye turkey beef meatball pancetta biltong jowl rump prosciutto.</p>
+            </div>
+            <div class="col-md-12 card">
+                <h5 class="h5">Berita Pertama Judul</h5>
+                <p>Bacon ipsum dolor amet corned beef swine burgdoggen, spare ribs short ribs pork porchetta venison t-bone pancetta beef ribeye. Pastrami frankfurter kielbasa flank, prosciutto tongue short loin alcatra filet mignon strip steak doner shank. Turducken shank meatloaf, swine cupim pork filet mignon jowl cow pig jerky tenderloin alcatra. Jerky buffalo boudin kevin. Pork loin pastrami brisket alcatra ground round t-bone burgdoggen leberkas shankle swine hamburger. Pastrami boudin buffalo short ribs strip steak ribeye turkey beef meatball pancetta biltong jowl rump prosciutto.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+<!-- jquery dan bootstrap js -->
+<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+
+</html>
