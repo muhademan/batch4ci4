15a4b53a0eba04cc9b5caa2d17b631b313139a26
muhademan <ade11rahman@gmail.com>
2020-12-20 15:17:59 +0700

    view baru

--- app/Views/template/layout.php
+++ app/Views/template/layout.php
@@ -10,30 +10,9 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container">
-            <a class="navbar-brand" href="#">Portal Berita</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <?= $this->include('template/navbar'); ?>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-
-
-                </ul>
-
-            </div>
-        </div>
-    </nav>
-
-    <?= $this->renderSection('content') ?>
+    <?= $this->renderSection('content'); ?>
 
     <footer class="footer mt-auto py-3">
         <div class="container">
